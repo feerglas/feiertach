783e82c5a4ef7ca0128a3000b69354ed21924c8e
feerglas <info@feerglas.com>
2020-10-10 11:20:08 +0200

    fix: fix issue with extracting locale

--- src/locales/extractLocale.js
+++ src/locales/extractLocale.js
@@ -1,30 +1,40 @@
-const _extractLocale = (prop, obj) => obj.constructor === Object && Object.keys(obj)
+const getDeepLocale = (prop, obj) => obj.constructor === Object && Object.keys(obj)
   .forEach((key) => {
     const __obj = obj;
     const keys = Object.keys(obj);
+    let shouldStop = false;
 
     keys.forEach((_key) => {
+      if (shouldStop) {
+        return;
+      }
+
       const _obj = obj[_key];
       const _keys = Object.keys(_obj);
 
       if (_keys.includes(prop)) {
+        shouldStop = true;
+
         const _value = obj[key][prop];
 
         delete __obj[key][prop];
 
         __obj[key] = _value;
+
+      } else {
+        shouldStop = false;
       }
     });
 
-    _extractLocale(prop, obj[key]);
+    getDeepLocale(prop, obj[key]);
   });
 
-const extractLocale = (prop, obj) => {
-  const _obj = JSON.parse(JSON.stringify(obj));
+const extractLocale = (prop, originalLocales) => {
+  const localesCopy = JSON.parse(JSON.stringify(originalLocales));
 
-  _extractLocale(prop, _obj);
+  getDeepLocale(prop, localesCopy);
 
-  return _obj;
+  return localesCopy;
 };
 
 export default extractLocale;
