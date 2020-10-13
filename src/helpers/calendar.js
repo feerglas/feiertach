ea60079dcda880edf9ef98bd39e8cb47c7cfab08
feerglas <info@feerglas.com>
2020-10-13 17:56:43 +0200

    feat: add method to get event object

--- src/helpers/calendar.js
+++ src/helpers/calendar.js
@@ -17,4 +17,31 @@ const addEvents = (events) => {
   download('feiertaCH', value);
 };
 
-export default addEvents;
+const getCalendarEventForHoliday = (holiday, locale, copyright, canton) => {
+  const event = {};
+  const date = [
+    parseInt(holiday.date.year, 10),
+    parseInt(holiday.date.month, 10),
+    parseInt(holiday.date.day, 10)
+  ];
+
+  event.start = date;
+  event.end = date;
+
+  event.title = `${holiday.title[locale]} (${canton})`;
+
+  if (holiday.description) {
+    event.description = `${holiday.description[locale]}\n\n${copyright}`;
+  } else {
+    event.description = copyright;
+  }
+
+  // console.log(event);
+
+  return event;
+};
+
+export {
+  addEvents,
+  getCalendarEventForHoliday
+};
