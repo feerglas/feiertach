199c9541047af43f24e362244f0a1a3267c25e04
feerglas <info@feerglas.com>
2020-10-13 17:55:02 +0200

    feat: add holidays to node collection

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,10 +1,34 @@
 const cantons = require('./src/data/cantons');
+const allHolidays = require('./src/data/holidays/allHolidays');
+const globalConfig = require('./src/config/global');
 
 const addCantonsCollection = (actions) => {
   const cantonsCollection = actions.addCollection('Canton');
+  const {
+    years
+  } = globalConfig;
 
   for (const canton of cantons) {
+    let holidays = [];
+
+    for (const year of years) {
+      const holidaysForYear = allHolidays.getHolidaysForYearAndCanton(year, canton.key);
+      const extractCantonInfo = holidaysForYear.map((holiday) => {
+        const cantonInfo = holiday.cantons[canton.key];
+        const _holiday = JSON.parse(JSON.stringify(holiday));
+
+        _holiday.allCanton = cantonInfo.allCanton;
+        _holiday.official = cantonInfo.official;
+        _holiday.half = cantonInfo.half;
+
+        return _holiday;
+      });
+
+      holidays = holidays.concat(extractCantonInfo);
+    }
+
     cantonsCollection.addNode({
+      holidays,
       id: canton.key,
       ...canton
     });
