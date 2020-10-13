9e7d0a4d9066b3f5ef7f038b6d9e9b8d03acf52a
feerglas <info@feerglas.com>
2020-10-13 09:12:38 +0200

    chore: add sample markup for canton

---
+++ src/templates/Canton.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout :title="$page.canton.name[this.$i18n.locale]">
+    {{$page.canton}}
+  </Layout>
+</template>
+
+<page-query>
+query ($id: ID!) {
+  canton(id: $id) {
+    flag
+    name {
+      en
+      de
+      it
+      fr
+    }
+    key
+  }
+}
+</page-query>
+
+<script>
+</script>
