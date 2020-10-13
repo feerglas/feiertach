6df2e59a9b54368661d466e38a9b284f29c33368
feerglas <info@feerglas.com>
2020-10-13 17:55:43 +0200

    chore: remove comment

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,44 +5,6 @@
 </template>
 
 <script>
-import addEvents from '../helpers/calendar';
-
-const event1 = {
-  description: 'Desc',
-  end: [
-    2020,
-    10,
-    7
-  ],
-  start: [
-    2020,
-    10,
-    7
-  ],
-  title: 'Title'
-};
-
-const event2 = {
-  description: 'Desc 2',
-  end: [
-    2020,
-    10,
-    8
-  ],
-  start: [
-    2020,
-    10,
-    8
-  ],
-  title: 'Title 2'
-};
-
-/*
-addEvents([
-  event1,
-  event2
-]);
-*/
 export default {
   metaInfo: {
     title: 'Hello, world!'
