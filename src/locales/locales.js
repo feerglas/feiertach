7274de56e38b3a6e94c469ebc6b44c1709ef0eaf
feerglas <info@feerglas.com>
2020-10-13 17:55:31 +0200

    feat: add required string locales

--- src/locales/locales.js
+++ src/locales/locales.js
@@ -19,6 +19,50 @@ const locales = {
       fr: 'About',
       it: 'About'
     }
+  },
+  holiday: {
+    addToCalendar: {
+      de: 'In den Kalender',
+      en: 'In den Kalender',
+      fr: 'In den Kalender',
+      it: 'In den Kalender'
+    },
+    addAllToCalendar: {
+      de: 'Alle in den Kalender',
+      en: 'Alle in den Kalender',
+      fr: 'Alle in den Kalender',
+      it: 'Alle in den Kalender'
+    },
+    copyright: {
+      de: 'Hinzugefügt mit feiertaCH: https://feerglas.github.io/feiertach/',
+      en: 'Hinzugefügt mit feiertaCH: https://feerglas.github.io/feiertach/',
+      fr: 'Hinzugefügt mit feiertaCH: https://feerglas.github.io/feiertach/',
+      it: 'Hinzugefügt mit feiertaCH: https://feerglas.github.io/feiertach/'
+    },
+    allCanton: {
+      de: 'Im ganzen Kanton',
+      en: 'Im ganzen Kanton',
+      fr: 'Im ganzen Kanton',
+      it: 'Im ganzen Kanton'
+    },
+    official: {
+      de: 'Gesetzlich anerkannt',
+      en: 'Gesetzlich anerkannt',
+      fr: 'Gesetzlich anerkannt',
+      it: 'Gesetzlich anerkannt'
+    },
+    date: {
+      de: 'Datum',
+      en: 'Datum',
+      fr: 'Datum',
+      it: 'Datum'
+    },
+    holiday: {
+      de: 'Feiertag',
+      en: 'Feiertag',
+      fr: 'Feiertag',
+      it: 'Feiertag'
+    }
   }
 };
 /* eslint-enable sort-keys */
