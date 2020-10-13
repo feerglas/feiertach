ea73fe4422c82839ef7cd7f3d97d580420808051
feerglas <info@feerglas.com>
2020-10-13 22:54:06 +0200

    fix: testing path prefix for svg path

--- src/templates/Canton.vue
+++ src/templates/Canton.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout
     :title="$page.canton.name[this.$i18n.locale]"
-    :image="'feiertach/images/flags/' + $page.canton.flag"
+    :image="'/feiertach/images/flags/' + $page.canton.flag"
     >
     <span>{{filteredHolidays.length}}</span>
 
