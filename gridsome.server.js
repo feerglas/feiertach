a961a30a723c20b829bbc0ec5c7db38d486cfdab
feerglas <info@feerglas.com>
2020-10-13 09:09:07 +0200

    feat: add collection for cantons

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,1 +1,18 @@
-module.exports = () => {};
+const cantons = require('./src/data/cantons');
+
+const addCantonsCollection = (actions) => {
+  const cantonsCollection = actions.addCollection('Canton');
+
+  for (const canton of cantons) {
+    cantonsCollection.addNode({
+      id: canton.key,
+      ...canton
+    });
+  }
+};
+
+module.exports = (api) => {
+  api.loadSource((actions) => {
+    addCantonsCollection(actions);
+  });
+};
