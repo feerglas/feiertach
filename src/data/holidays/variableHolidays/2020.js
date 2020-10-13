1ffa170adb9edaeea18c45239c00e0a2ab48519f
feerglas <info@feerglas.com>
2020-10-13 09:11:00 +0200

    feat: add data for variable holidays in 2020

---
+++ src/data/holidays/variableHolidays/2020.js
@@ -0,0 +1,890 @@
+const holidays = [
+  {
+    cantons: {
+      ag: {
+        allCanton: true,
+        official: true
+      },
+      ai: {
+        allCanton: true,
+        official: true
+      },
+      ar: {
+        allCanton: true,
+        official: true
+      },
+      be: {
+        allCanton: true,
+        official: true
+      },
+      bl: {
+        allCanton: true,
+        official: true
+      },
+      bs: {
+        allCanton: true,
+        official: true
+      },
+      fr: {
+        allCanton: true,
+        official: true
+      },
+      ge: {
+        allCanton: true,
+        official: true
+      },
+      gl: {
+        allCanton: true,
+        official: true
+      },
+      gr: {
+        allCanton: true,
+        official: true
+      },
+      ju: {
+        allCanton: true,
+        official: true
+      },
+      lu: {
+        allCanton: true,
+        official: true
+      },
+      ne: {
+        allCanton: true,
+        official: true
+      },
+      nw: {
+        allCanton: true,
+        official: true
+      },
+      ow: {
+        allCanton: true,
+        official: true
+      },
+      sg: {
+        allCanton: true,
+        official: true
+      },
+      sh: {
+        allCanton: true,
+        official: true
+      },
+      so: {
+        allCanton: true,
+        official: true
+      },
+      sz: {
+        allCanton: true,
+        official: true
+      },
+      tg: {
+        allCanton: true,
+        official: true
+      },
+      ti: {
+        allCanton: true,
+        official: true
+      },
+      ur: {
+        allCanton: true,
+        official: true
+      },
+      vd: {
+        allCanton: true,
+        official: true
+      },
+      vs: {
+        allCanton: true,
+        official: true
+      },
+      zg: {
+        allCanton: true,
+        official: true
+      },
+      zh: {
+        allCanton: true,
+        official: true
+      }
+    },
+    date: {
+      day: 21,
+      month: 5
+    },
+    description: {
+      de: '40 Tage nach Ostern',
+      en: '40 days past easter',
+      fr: '40 jours après Pâques',
+      it: '40 giorni dopo Pasqua'
+    },
+    links: {
+      de: 'http://de.m.wikipedia.org/wiki/Christi_Himmelfahrt',
+      en: 'http://de.m.wikipedia.org/wiki/Christi_Himmelfahrt',
+      fr: 'https://fr.m.wikipedia.org/wiki/Ascension_(f%C3%AAte)',
+      it: 'http://it.m.wikipedia.org/wiki/Ascensione'
+    },
+    title: {
+      de: 'Auffahrt',
+      en: 'Auffahrt',
+      fr: 'Ascension',
+      it: 'Ascensione'
+    }
+  },
+  {
+    cantons: {
+      be: {
+        allCanton: false,
+        official: false
+      },
+      ne: {
+        allCanton: false,
+        official: false
+      },
+      vd: {
+        allCanton: true,
+        official: true
+      },
+      vs: {
+        allCanton: false,
+        official: false
+      }
+    },
+    date: {
+      day: 21,
+      month: 9
+    },
+    description: {
+      de: 'am Montag nach dem Eidgenössischen Dank-, Buss- und Bettag',
+      en: 'the Monday after the Eidgenössischen Dank-, Buss- und Bettag',
+      fr: 'le lundi après Jeûne fédéral',
+      it: 'il Lunedi dopo che  Eidgenössischer Dank-, Buss- und Bettag'
+    },
+    links: {
+      de: 'http://de.m.wikipedia.org/wiki/Eidgenössischer_Dank-,_Buss-_und_Bettag',
+      en: 'http://de.m.wikipedia.org/wiki/Eidgenössischer_Dank-,_Buss-_und_Bettag',
+      fr: 'http://de.m.wikipedia.org/wiki/Eidgenössischer_Dank-,_Buss-_und_Bettag',
+      it: 'http://de.m.wikipedia.org/wiki/Eidgenössischer_Dank-,_Buss-_und_Bettag'
+    },
+    title: {
+      de: 'Bettagsmontag',
+      en: 'Bettagsmontag',
+      fr: 'Lundi du Jeûne fédéral',
+      it: 'Bettagsmontag'
+    }
+  },
+  {
+    cantons: {
+      ag: {
+        allCanton: false,
+        official: true
+      },
+      ai: {
+        allCanton: true,
+        official: true
+      },
+      bl: {
+        allCanton: false,
+        official: true
+      },
+      fr: {
+        allCanton: false,
+        official: true
+      },
+      gr: {
+        allCanton: false,
+        official: true
+      },
+      ju: {
+        allCanton: true,
+        official: true
+      },
+      lu: {
+        allCanton: true,
+        official: true
+      },
+      ne: {
+        allCanton: false,
+        official: true
+      },
+      nw: {
+        allCanton: true,
+        official: true
+      },
+      ow: {
+        allCanton: true,
+        official: true
+      },
+      so: {
+        allCanton: false,
+        official: true
+      },
+      sz: {
+        allCanton: true,
+        official: true
+      },
+      ti: {
+        allCanton: true,
+        official: true
+      },
+      ur: {
+        allCanton: true,
+        official: true
+      },
+      vs: {
+        allCanton: true,
+        official: true
+      },
+      zg: {
+        allCanton: true,
+        official: true
+      }
+    },
+    date: {
+      day: 11,
+      month: 6
+    },
+    description: {
+      de: '60 Tage nach Ostern',
+      en: '60 days past easter',
+      fr: '60 jours après Pâques',
+      it: '60 giorni dopo Pasqua'
+    },
+    links: {
+      de: 'http://de.m.wikipedia.org/wiki/Fronleichnam',
+      en: 'http://en.m.wikipedia.org/wiki/Corpus_Christi_(feast)',
+      fr: 'http://fr.m.wikipedia.org/wiki/Fête-Dieu',
+      it: 'http://it.m.wikipedia.org/wiki/Corpus_Domini'
+    },
+    title: {
+      de: 'Fronleichnam',
+      en: 'Corpus Christi',
+      fr: 'Fête-Dieu',
+      it: 'Corpus Domini'
+    }
+  },
+  {
+    cantons: {
+      ge: {
+        allCanton: true,
+        official: false
+      }
+    },
+    date: {
+      day: 10,
+      month: 9
+    },
+    description: {
+      de: 'am Donnerstag nach dem ersten Sonntag im September',
+      en: 'on Thursday after the first Sunday in September',
+      fr: 'le jeudi après le premier dimanche en Septembre',
+      it: 'il Giovedi dopo la prima Domenica di settembre'
+    },
+    links: {
+      de: 'http://de.m.wikipedia.org/wiki/Genfer_Bettag',
+      en: 'http://de.m.wikipedia.org/wiki/Genfer_Bettag',
+      fr: 'http://de.m.wikipedia.org/wiki/Genfer_Bettag',
+      it: 'http://de.m.wikipedia.org/wiki/Genfer_Bettag'
+    },
+    title: {
+      de: 'Genfer Bettag',
+      en: 'Jeûne genevois',
+      fr: 'Jeûne genevois',
+      it: 'Jeûne genevois'
+    }
+  },
+  {
+    cantons: {
+      ag: {
+        allCanton: true,
+        official: true
+      },
+      ai: {
+        allCanton: true,
+        official: true
+      },
+      ar: {
+        allCanton: true,
+        official: true
+      },
+      be: {
+        allCanton: true,
+        official: true
+      },
+      bl: {
+        allCanton: true,
+        official: true
+      },
+      bs: {
+        allCanton: true,
+        official: true
+      },
+      fr: {
+        allCanton: true,
+        official: true
+      },
+      ge: {
+        allCanton: true,
+        official: true
+      },
+      gl: {
+        allCanton: true,
+        official: true
+      },
+      gr: {
+        allCanton: true,
+        official: true
+      },
+      ju: {
+        allCanton: true,
+        official: true
+      },
+      lu: {
+        allCanton: true,
+        official: true
+      },
+      ne: {
+        allCanton: true,
+        official: true
+      },
+      nw: {
+        allCanton: true,
+        official: true
+      },
+      ow: {
+        allCanton: true,
+        official: true
+      },
+      sg: {
+        allCanton: true,
+        official: true
+      },
+      sh: {
+        allCanton: true,
+        official: true
+      },
+      so: {
+        allCanton: true,
+        official: true
+      },
+      sz: {
+        allCanton: true,
+        official: true
+      },
+      tg: {
+        allCanton: true,
+        official: true
+      },
+      ur: {
+        allCanton: true,
+        official: true
+      },
+      vd: {
+        allCanton: true,
+        official: true
+      },
+      zg: {
+        allCanton: true,
+        official: true
+      },
+      zh: {
+        allCanton: true,
+        official: true
+      }
+    },
+    date: {
+      day: 10,
+      month: 4
+    },
+    description: {
+      de: 'am Freitag vor Ostern',
+      en: 'on Friday before easter',
+      fr: 'le vendredi précédant le dimanche de Pâques',
+      it: 'il venerdì che precede la Pasqua cristiana'
+    },
+    links: {
+      de: 'http://de.m.wikipedia.org/wiki/Karfreitag',
+      en: 'http://en.m.wikipedia.org/wiki/Good_friday',
+      fr: 'http://fr.m.wikipedia.org/wiki/Vendredi_saint',
+      it: 'http://it.m.wikipedia.org/wiki/Venerdì_santo'
+    },
+    title: {
+      de: 'Karfreitag',
+      en: 'Good Friday',
+      fr: 'Vendredi Saint',
+      it: 'Venerdì santo'
+    }
+  },
+  {
+    cantons: {
+      zh: {
+        allCanton: false,
+        official: false
+      }
+    },
+    date: {
+      day: 14,
+      month: 9
+    },
+    description: {
+      de: 'am zweiten Wochenende im September',
+      en: 'the second weekend of September',
+      fr: 'le deuxième week-end en Septembre',
+      it: 'il secondo fine settimana di settembre'
+    },
+    links: {
+      de: 'http://de.m.wikipedia.org/wiki/Knabenschiessen',
+      en: 'http://de.m.wikipedia.org/wiki/Knabenschiessen',
+      fr: 'http://de.m.wikipedia.org/wiki/Knabenschiessen',
+      it: 'http://de.m.wikipedia.org/wiki/Knabenschiessen'
+    },
+    title: {
+      de: 'Knabenschiessen',
+      en: 'Knabenschiessen',
+      fr: 'Knabenschiessen',
+      it: 'Knabenschiessen'
+    }
+  },
+  {
+    cantons: {
+      gl: {
+        allCanton: true,
+        official: false
+      }
+    },
+    date: {
+      day: 2,
+      month: 4
+    },
+    description: {
+      de: 'am ersten Donnerstag im April (wenn Näfelser Fahrt in die Karwoche fällt, wird die Fahrt um eine Woche nach hinten verschoben)',
+      en: 'the first Thursday in April',
+      fr: 'le premier jeudi d\'avril',
+      it: 'il primo Giovedi nel mese di aprile'
+    },
+    links: {
+      de: 'http://de.m.wikipedia.org/wiki/Näfelser_Fahrt',
+      en: 'http://de.m.wikipedia.org/wiki/Näfelser_Fahrt',
+      fr: 'http://de.m.wikipedia.org/wiki/Näfelser_Fahrt',
+      it: 'http://de.m.wikipedia.org/wiki/Näfelser_Fahrt'
+    },
+    title: {
+      de: 'Näfelser Fahrt',
+      en: 'Näfelser Fahrt',
+      fr: 'Näfelser Fahrt',
+      it: 'Näfelser Fahrt'
+    }
+  },
+  {
+    cantons: {
+      ag: {
+        allCanton: false,
+        official: true
+      },
+      ai: {
+        allCanton: true,
+        official: true
+      },
+      ar: {
+        allCanton: true,
+        official: true
+      },
+      be: {
+        allCanton: true,
+        official: true
+      },
+      bl: {
+        allCanton: true,
+        official: true
+      },
+      bs: {
+        allCanton: true,
+        official: true
+      },
+      fr: {
+        allCanton: true,
+        official: false
+      },
+      ge: {
+        allCanton: true,
+        official: true
+      },
+      gl: {
+        allCanton: true,
+        official: true
+      },
+      gr: {
+        allCanton: true,
+        official: true
+      },
+      ju: {
+        allCanton: true,
+        official: true
+      },
+      lu: {
+        allCanton: true,
+        official: false
+      },
+      ne: {
+        allCanton: true,
+        official: false
+      },
+      nw: {
+        allCanton: true,
+        official: false
+      },
+      ow: {
+        allCanton: true,
+        official: false
+      },
+      sg: {
+        allCanton: true,
+        official: true
+      },
+      sh: {
+        allCanton: true,
+        official: true
+      },
+      so: {
+        allCanton: false,
+        official: true
+      },
+      sz: {
+        allCanton: true,
+        official: true
+      },
+      tg: {
+        allCanton: true,
+        official: true
+      },
+      ti: {
+        allCanton: true,
+        official: true
+      },
+      ur: {
+        allCanton: true,
+        official: true
+      },
+      vd: {
+        allCanton: true,
+        official: true
+      },
+      zg: {
+        allCanton: true,
+        official: false
+      },
+      zh: {
+        allCanton: true,
+        official: true
+      }
+    },
+    date: {
+      day: 13,
+      month: 4
+    },
+    description: {
+      de: 'am Montag nach Ostern',
+      en: 'on Monday after easter',
+      fr: 'le lendemain du jour de Pâques',
+      it: 'il giorno dopo la Pasqua'
+    },
+    links: {
+      de: 'http://de.m.wikipedia.org/wiki/Ostermontag',
+      en: 'http://en.m.wikipedia.org/wiki/Easter_Monday',
+      fr: 'http://fr.m.wikipedia.org/wiki/Lundi_de_Pâques',
+      it: 'http://it.m.wikipedia.org/wiki/Lunedì_di_Pasqua'
+    },
+    title: {
+      de: 'Ostermontag',
+      en: 'Easter Monday',
+      fr: 'Lundi de Pâques',
+      it: 'Lunedì di Pasqua'
+    }
+  },
+  {
+    cantons: {
+      ag: {
+        allCanton: true,
+        official: true
+      },
+      ai: {
+        allCanton: true,
+        official: true
+      },
+      ar: {
+        allCanton: true,
+        official: true
+      },
+      be: {
+        allCanton: true,
+        official: true
+      },
+      bl: {
+        allCanton: true,
+        official: true
+      },
+      bs: {
+        allCanton: true,
+        official: true
+      },
+      fr: {
+        allCanton: true,
+        official: true
+      },
+      ge: {
+        allCanton: true,
+        official: true
+      },
+      gl: {
+        allCanton: true,
+        official: true
+      },
+      gr: {
+        allCanton: true,
+        official: true
+      },
+      ju: {
+        allCanton: true,
+        official: true
+      },
+      lu: {
+        allCanton: true,
+        official: true
+      },
+      ne: {
+        allCanton: true,
+        official: true
+      },
+      nw: {
+        allCanton: true,
+        official: true
+      },
+      ow: {
+        allCanton: true,
+        official: true
+      },
+      sg: {
+        allCanton: true,
+        official: true
+      },
+      sh: {
+        allCanton: true,
+        official: true
+      },
+      so: {
+        allCanton: true,
+        official: true
+      },
+      sz: {
+        allCanton: true,
+        official: true
+      },
+      tg: {
+        allCanton: true,
+        official: true
+      },
+      ti: {
+        allCanton: true,
+        official: true
+      },
+      ur: {
+        allCanton: true,
+        official: true
+      },
+      vd: {
+        allCanton: true,
+        official: true
+      },
+      vs: {
+        allCanton: true,
+        official: true
+      },
+      zg: {
+        allCanton: true,
+        official: true
+      },
+      zh: {
+        allCanton: true,
+        official: true
+      }
+    },
+    date: {
+      day: 12,
+      month: 4
+    },
+    description: {
+      de: 'am Sonntag nach dem ersten Frühjahrsvollmond',
+      en: 'on Sunday after the first spring full moon',
+      fr: 'le dimanche après la première pleine lune de printemps',
+      it: 'la Domenica dopo la luna piena prima molla'
+    },
+    links: {
+      de: 'http://de.m.wikipedia.org/wiki/Ostern',
+      en: 'http://en.m.wikipedia.org/wiki/Easter',
+      fr: 'http://fr.m.wikipedia.org/wiki/Pâques',
+      it: 'http://it.m.wikipedia.org/wiki/Pasqua'
+    },
+    title: {
+      de: 'Ostersonntag',
+      en: 'Easter',
+      fr: 'Pâques',
+      it: 'Pasqua'
+    }
+  },
+  {
+    cantons: {
+      ag: {
+        allCanton: false,
+        official: true
+      },
+      ai: {
+        allCanton: true,
+        official: true
+      },
+      ar: {
+        allCanton: true,
+        official: true
+      },
+      be: {
+        allCanton: true,
+        official: true
+      },
+      bl: {
+        allCanton: true,
+        official: true
+      },
+      bs: {
+        allCanton: true,
+        official: true
+      },
+      fr: {
+        allCanton: true,
+        official: false
+      },
+      ge: {
+        allCanton: true,
+        official: true
+      },
+      gl: {
+        allCanton: true,
+        official: true
+      },
+      gr: {
+        allCanton: true,
+        official: true
+      },
+      ju: {
+        allCanton: true,
+        official: true
+      },
+      lu: {
+        allCanton: true,
+        official: false
+      },
+      ne: {
+        allCanton: true,
+        official: false
+      },
+      nw: {
+        allCanton: true,
+        official: false
+      },
+      ow: {
+        allCanton: true,
+        official: false
+      },
+      sg: {
+        allCanton: true,
+        official: true
+      },
+      sh: {
+        allCanton: true,
+        official: true
+      },
+      so: {
+        allCanton: false,
+        official: true
+      },
+      sz: {
+        allCanton: true,
+        official: true
+      },
+      tg: {
+        allCanton: true,
+        official: true
+      },
+      ti: {
+        allCanton: true,
+        official: true
+      },
+      ur: {
+        allCanton: true,
+        official: true
+      },
+      vd: {
+        allCanton: true,
+        official: true
+      },
+      zg: {
+        allCanton: true,
+        official: false
+      },
+      zh: {
+        allCanton: true,
+        official: true
+      }
+    },
+    date: {
+      day: 1,
+      month: 6
+    },
+    description: {
+      de: 'am Montag nach Pfingsten',
+      en: 'on Monday after Pentecost',
+      fr: 'le lundi après la Pentecôte',
+      it: 'il Lunedi dopo la Pentecoste'
+    },
+    links: {
+      de: 'http://de.m.wikipedia.org/wiki/Pfingsten',
+      en: 'http://en.m.wikipedia.org/wiki/Pentecost',
+      fr: 'http://fr.m.wikipedia.org/wiki/Pentecôte',
+      it: 'http://it.m.wikipedia.org/wiki/Pentecoste'
+    },
+    title: {
+      de: 'Pfingstmontag',
+      en: 'Monday of Pentecost',
+      fr: 'Lundi de Pentecôte',
+      it: 'Lunedì di Pentecoste'
+    }
+  },
+  {
+    cantons: {
+      zh: {
+        allCanton: false,
+        official: false
+      }
+    },
+    date: {
+      day: 20,
+      month: 4
+    },
+    description: {
+      de: 'am dritten Montag im April',
+      en: 'the thirt Monday in April',
+      fr: 'sur le troisième lundi d\'avril',
+      it: 'sul terzo Lunedi nel mese di aprile'
+    },
+    links: {
+      de: 'http://de.m.wikipedia.org/wiki/Sechseläuten',
+      en: 'http://de.m.wikipedia.org/wiki/Sechseläuten',
+      fr: 'http://de.m.wikipedia.org/wiki/Sechseläuten',
+      it: 'http://de.m.wikipedia.org/wiki/Sechseläuten'
+    },
+    title: {
+      de: 'Sechseläuten',
+      en: 'Sechseläuten',
+      fr: 'Sechseläuten',
+      it: 'Sechseläuten'
+    }
+  }
+];
+
+export default holidays;
---
+++ src/data/holidays/variableHolidays/index.js
@@ -0,0 +1,7 @@
+import holidays2020 from './2020';
+
+const holidays = {
+  holidays2020
+};
+
+export default holidays;
