ae7d0add8f8595ccfe13c68ce8accf9273d4c594
feerglas <info@feerglas.com>
2020-10-15 15:34:07 +0200

    feat: add content and stylings

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,63 @@
 <template>
 
   <footer class="footer">
-    <div class="content">
+    <div class="container">
+      <div class="content is-small">
 
-      footer
+        <div class="block">
+          <p>©2020 Yves Tscherry</p>
+          <a
+            href="https://github.com/feerglas/feiertach"
+            target="_blank"
+          >{{$t('footer.findOnGithub')}}</a>
+        </div>
 
+        <div class="block social-links">
+          <p>{{$t('footer.findOnSocial')}}</p>
+          <SocialLinks />
+        </div>
+
+      </div>
     </div>
   </footer>
 
 </template>
 
 <script>
+import SocialLinks from './SocialLinks.vue';
 
 export default {
+  components: {
+    SocialLinks
+  },
   name: 'Footer'
 };
 
 </script>
+
+<style lang="scss" scoped>
+@import "../styles/bulma.scss";
+
+.footer.footer {
+  padding-left: 0;
+  padding-right: 0;
+  text-align: center;
+}
+
+.container {
+  @include until($desktop) {
+    padding: 0 1rem;
+  }
+
+  @include from($desktop) {
+    max-width: calc(960px - (2 * #{$gap}));
+    padding: 0;
+  }
+
+  .social-links ul {
+    list-style-type: none;
+    margin-left: 0;
+  }
+}
+
+</style>
