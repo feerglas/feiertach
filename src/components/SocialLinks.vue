d9e45da291ae64bd607c5e497b0c1c5a5cdb6ed3
feerglas <info@feerglas.com>
2020-10-15 15:34:47 +0200

    refactor: add component for social links

---
+++ src/components/SocialLinks.vue
@@ -0,0 +1,42 @@
+<template>
+
+  <ul>
+    <li>
+      <a
+        href="https://twitter.com/feerglas"
+        target="_blank"
+      >
+        Twitter
+      </a>
+    </li>
+
+    <li>
+      <a
+        href="https://www.facebook.com/yves.tscherry"
+        target="_blank"
+      >
+        Facbeook
+      </a>
+    </li>
+
+    <li>
+      <a
+        href="https://www.instagram.com/feerglas"
+        target="_blank"
+      >
+        Instagram
+      </a>
+    </li>
+  </ul>
+
+</template>
+
+<script>
+export default {
+  name: 'SocialLinks'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
