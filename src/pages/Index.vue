4cf8052179fc3908d2ff9d4ccca69c998051495a
feerglas <info@feerglas.com>
2020-10-15 08:03:35 +0200

    feat: add content to home

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,31 @@
 <template>
   <Layout title="feiertaCH">
+    <div class="content is-medium">
+      <p class="is-size-4">{{$t("home.lead")}}</p>
 
+      <h2 class="title is-2">{{$t("home.title1")}}</h2>
+      <p>{{$t("home.content1")}}</p>
+
+      <ul>
+        <li><a href="https://twitter.com/feerglas" target="_blank">Twitter</a></li>
+        <li><a href="https://www.facebook.com/yves.tscherry" target="_blank">Facbeook</a></li>
+        <li><a href="https://www.instagram.com/feerglas" target="_blank">Instagram</a></li>
+      </ul>
+
+      <h2 class="title is-2">{{$t("home.title2")}}</h2>
+      <p>{{$t("home.content2")}}</p>
+      <ul>
+        <li><g-link to="/cantons/">{{$t("navigation.cantons")}}</g-link></li>
+        <li><g-link to="/holidays/">{{$t("navigation.holidays")}}</g-link></li>
+      </ul>
+    </div>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'feiertaCH'
   }
 };
 </script>
