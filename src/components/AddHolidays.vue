ea9d5ea2ea6b182949d13163a070e2796fbaf1a3
feerglas <info@feerglas.com>
2020-10-15 11:28:49 +0200

    refactor: add component for add to calendar button

---
+++ src/components/AddHolidays.vue
@@ -0,0 +1,28 @@
+<template>
+  <a
+    @click="addAllEvents(holidays, canton)"
+  >{{$t('holiday.addAllToCalendar')}}</a>
+</template>
+
+<script>
+import { addEventsForHolidays } from '../helpers/calendar';
+
+export default {
+  data() {
+    return {
+      currentLocale: this.$i18n.locale
+    };
+  },
+  methods: {
+    addAllEvents(holidays, canton) {
+      addEventsForHolidays(holidays, this.currentLocale, this.$t('holiday.copyright'), canton);
+    }
+  },
+  name: 'AddHolidays',
+  props: [
+    'holidays',
+    'canton'
+  ]
+};
+
+</script>
