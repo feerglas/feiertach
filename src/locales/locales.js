d74a8b8c14e6a155d0ff93a0fe171509a97ca857
feerglas <info@feerglas.com>
2020-10-18 22:21:16 +0200

    feat: add translations for en, it, fr

--- src/locales/locales.js
+++ src/locales/locales.js
@@ -4,136 +4,136 @@ const locales = {
     home: {
       de: 'Start',
       en: 'Home',
-      fr: 'Home',
+      fr: 'Accueil',
       it: 'Home'
     },
     cantons: {
       de: 'Kantone',
       en: 'Cantons',
-      fr: 'Kantone',
-      it: 'Kantone'
+      fr: 'Cantons',
+      it: 'Cantoni'
     },
     holidays: {
       de: 'Feiertage',
       en: 'Holidays',
-      fr: 'Feiertage',
-      it: 'Feiertage'
+      fr: 'Vacances',
+      it: 'Feste'
     }
   },
   footer: {
     findOnGithub: {
-      de: 'Project on GitHub',
+      de: 'Projekt auf GitHub',
       en: 'Project on GitHub',
-      fr: 'Project on GitHub',
-      it: 'Project on GitHub'
+      fr: 'Projet sur GitHub',
+      it: 'Progetto su GitHub'
     },
     findOnSocial: {
-      de: 'Find me on',
+      de: 'Finde mich auf',
       en: 'Find me on',
-      fr: 'Find me on',
-      it: 'Find me on'
+      fr: 'Trouve-moi sur',
+      it: 'Trovami su'
     }
   },
   home: {
     lead: {
       de: 'Mit feiertaCH kannst du Feiertage für die einzelnen Kantone direkt in den Kalender eintragen.',
-      en: 'Mit feiertaCH kannst du Feiertage für die einzelnen Kantone direkt in den Kalender eintragen.',
-      fr: 'Mit feiertaCH kannst du Feiertage für die einzelnen Kantone direkt in den Kalender eintragen.',
-      it: 'Mit feiertaCH kannst du Feiertage für die einzelnen Kantone direkt in den Kalender eintragen.'
+      en: 'With feiertaCH you can add public holidays for the individual cantons directly into your calendar.',
+      fr: 'Avec feiertaCH, tu peux saisir les jours fériés des cantons individuels directement dans le calendrier.',
+      it: 'Con feiertaCH è possibile inserire i giorni festivi dei singoli cantoni direttamente nel calendario.'
     },
     title1: {
       de: 'Trommelwirbel',
-      en: 'Trommelwirbel',
-      fr: 'Trommelwirbel',
-      it: 'Trommelwirbel'
+      en: 'Drum roll',
+      fr: 'Roulement de tambour',
+      it: 'Rullo di tamburi'
     },
     content1: {
       de: 'Diese App ist werbefrei, und du kannst sie gratis benutzen. Wenn du mir "Merci" sagen willst, wenn du Verbesserungsvorschläge hast oder Fehler gefunden hast, kannst du dich gerne via Twitter, Facebook oder Insta bei mir melden:',
-      en: 'Diese App ist werbefrei, und du kannst sie umsonst benutzen. Wenn du mir "Merci" sagen willst, wenn du Verbesserungsvorschläge hast oder Fehler gefunden hast, kannst du dich gerne via Twitter, Facebook oder Insta bei mir melden:',
-      fr: 'Diese App ist werbefrei, und du kannst sie umsonst benutzen. Wenn du mir "Merci" sagen willst, wenn du Verbesserungsvorschläge hast oder Fehler gefunden hast, kannst du dich gerne via Twitter, Facebook oder Insta bei mir melden:',
-      it: 'Diese App ist werbefrei, und du kannst sie umsonst benutzen. Wenn du mir "Merci" sagen willst, wenn du Verbesserungsvorschläge hast oder Fehler gefunden hast, kannst du dich gerne via Twitter, Facebook oder Insta bei mir melden:'
+      en: 'This app is ad-free and you can use it for free. If you want to tell me "Merci", if you have suggestions for improvements or if you have found errors, feel free to contact me via Twitter, Facebook or Insta:',
+      fr: 'Cette application est sans publicité et tu peux l\'utiliser gratuitement. Si tu souhaite me dire "Merci", si tu as des suggestions d\'amélioration ou si tu as trouvé des erreurs, n\'hésite pas à me contacter via Twitter, Facebook ou Insta:',
+      it: 'Questa app è senza pubblicità e puoi usarla gratuitamente. Se vuoi dirmi "Gracie", se hai suggerimenti per il miglioramento o se hai trovato errori, non esitare a contattarmi tramite Twitter, Facebook o Insta:'
     },
     title2: {
       de: 'Los geht\'s',
-      en: 'Los geht\'s',
-      fr: 'Los geht\'s',
-      it: 'Los geht\'s'
+      en: 'Here we go',
+      fr: 'Allons-y',
+      it: 'Andiamo'
     },
     content2: {
       de: 'Wähle einen Kanton oder schau dir die Feiertage sämtlicher Kantone an.',
-      en: 'Wähle einen Kanton oder schau dir die Feiertage sämtlicher Kantone an.',
-      fr: 'Wähle einen Kanton oder schau dir die Feiertage sämtlicher Kantone an.',
-      it: 'Wähle einen Kanton oder schau dir die Feiertage sämtlicher Kantone an.'
+      en: 'Choose a canton or look at the public holidays of all cantons.',
+      fr: 'Choisi un canton ou regarde les jours fériés de tous les cantons.',
+      it: 'Scegli un cantone o guarda i giorni festivi di tutti i cantoni.'
     }
   },
   holidays: {
     cantons: {
       de: 'Kantone',
-      en: 'Kantone',
-      fr: 'Kantone',
-      it: 'Kantone'
+      en: 'Cantons',
+      fr: 'Cantons',
+      it: 'Cantoni'
     }
   },
   holiday: {
     addToCalendar: {
       de: 'Kalender',
-      en: 'Kalender',
-      fr: 'Kalender',
-      it: 'Kalender'
+      en: 'Calendar',
+      fr: 'Calendrier',
+      it: 'Calendario'
     },
     addAllToCalendar: {
       de: 'Alle in den Kalender',
-      en: 'Alle in den Kalender',
-      fr: 'Alle in den Kalender',
-      it: 'Alle in den Kalender'
+      en: 'Add all to calendar',
+      fr: 'Tout dans le calendrier',
+      it: 'Tutto nel calendario'
     },
     copyright: {
       de: 'Hinzugefügt mit feiertaCH: https://feerglas.github.io/feiertach/',
-      en: 'Hinzugefügt mit feiertaCH: https://feerglas.github.io/feiertach/',
-      fr: 'Hinzugefügt mit feiertaCH: https://feerglas.github.io/feiertach/',
-      it: 'Hinzugefügt mit feiertaCH: https://feerglas.github.io/feiertach/'
+      en: 'Added with feiertaCH: https://feerglas.github.io/feiertach/',
+      fr: 'Ajouté avec feiertaCH: https://feerglas.github.io/feiertach/',
+      it: 'Aggiunto con feiertaCH: https://feerglas.github.io/feiertach/'
     },
     allCanton: {
       de: 'Überall',
-      en: 'Überall',
-      fr: 'Überall',
-      it: 'Überall'
+      en: 'Everywhere',
+      fr: 'Partout',
+      it: 'Ovunque'
     },
     allCantonExplanation: {
       de: 'Gibt an, ob der Feiertag im ganzen Kanton gilt oder nicht',
-      en: 'Gibt an, ob der Feiertag im ganzen Kanton gilt oder nicht',
-      fr: 'Gibt an, ob der Feiertag im ganzen Kanton gilt oder nicht',
-      it: 'Gibt an, ob der Feiertag im ganzen Kanton gilt oder nicht'
+      en: 'Indicates whether the public holiday applies to the entire canton or not',
+      fr: 'Indique si le jour férié s\'applique à tout le canton ou non',
+      it: 'Indica se il giorno festivo si applica o meno all\'intero cantone'
     },
     official: {
       de: 'Gesetzlich',
-      en: 'Gesetzlich',
-      fr: 'Gesetzlich',
-      it: 'Gesetzlich'
+      en: 'Legally',
+      fr: 'Légalement',
+      it: 'Legalmente'
     },
     officialExplanation: {
       de: 'Gibt an, ob der Feiertag gesetzlich anerkannt ist oder nicht',
-      en: 'Gibt an, ob der Feiertag gesetzlich anerkannt ist oder nicht',
-      fr: 'Gibt an, ob der Feiertag gesetzlich anerkannt ist oder nicht',
-      it: 'Gibt an, ob der Feiertag gesetzlich anerkannt ist oder nicht'
+      en: 'Indicates whether the holiday is legally recognized or not',
+      fr: 'Indique si le jour férié est légalement reconnu ou non',
+      it: 'Indica se la vacanza è legalmente riconosciuta o meno'
     },
     date: {
       de: 'Datum',
-      en: 'Datum',
-      fr: 'Datum',
-      it: 'Datum'
+      en: 'Date',
+      fr: 'Date',
+      it: 'Data'
     },
     holiday: {
       de: 'Feiertag',
-      en: 'Feiertag',
-      fr: 'Feiertag',
-      it: 'Feiertag'
+      en: 'Holiday',
+      fr: 'Vacance',
+      it: 'Fest'
     },
     infos: {
       de: 'Infos',
-      en: 'Infos',
+      en: 'Info',
       fr: 'Infos',
-      it: 'Infos'
+      it: 'Informazioni'
     }
   }
 };
