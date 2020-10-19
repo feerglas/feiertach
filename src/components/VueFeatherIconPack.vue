67e2519bb3bce9ae63cbba17e297232abaf1ccae
feerglas <info@feerglas.com>
2020-10-19 16:46:38 +0200

    fix: add component to use feather icons

---
+++ src/components/VueFeatherIconPack.vue
@@ -0,0 +1,45 @@
+<template>
+  <component class="custom-feather-icon" :is="iconComponent"/>
+</template>
+
+<script>
+import {
+  AlertTriangleIcon,
+  CheckIcon,
+  ChevronUpIcon,
+  GlobeIcon,
+  InfoIcon,
+  PlusIcon,
+  XIcon
+} from 'vue-feather-icons';
+
+export default {
+  components: {
+    AlertTriangleIcon,
+    CheckIcon,
+    ChevronUpIcon,
+    GlobeIcon,
+    InfoIcon,
+    PlusIcon,
+    XIcon
+  },
+  computed: {
+    iconComponent() {
+      return `${this.icon[1]}Icon`;
+    }
+  },
+  props: [
+    'icon',
+    'size',
+    'customClass'
+  ]
+};
+</script>
+
+<style  lang="scss">
+  .icon .custom-feather-icon {
+    fill: none;
+    stroke: currentColor;
+    stroke-width: 2px;
+  }
+</style>
