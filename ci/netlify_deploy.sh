bf948b4cb0463b2f6ea2891ef215fed316ee21ac
feerglas <info@feerglas.com>
2020-10-19 11:38:35 +0200

    build: push version as deploy message to netlify

---
+++ ci/netlify_deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# this script is used in .travis.yml
+
+# this script is being made executable with
+# chmod ugo+x netlify_deploy.sh
+
+# set flags for execution of this file:
+# -e: make sure command returns non-zero exit code as soon as somehting fails
+set -e
+
+# version file name written by .releaserc
+VERSION_FILE=./dist/.version
+
+if [ -f "$VERSION_FILE" ];
+then
+
+  VERSION="$(cat .version)"
+
+  # Deploy Storybook on Netlify
+  netlify deploy --prod --message "::$VERSION::" --site $NETLIFY_SITE_ID --auth $NETLIFY_AUTH_TOKEN --dir ./dist/
+
+else
+  echo "-->> Skipping netlify depoly"
+fi
