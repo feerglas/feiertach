d84185b1123f007ba9979b92cbd307fc6f0719f8
feerglas <info@feerglas.com>
2020-10-19 18:26:24 +0200

    refactor: add component for holiday info line

---
+++ src/components/HolidaysInfoLine.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="comp-info-line">
+    <span class="holidays-counter tag is-primary">{{holidays.length}} {{$t('navigation.holidays')}}</span>
+
+    <AddHolidays
+      :holidays="holidays"
+      :canton="canton"
+      class="add-all-button"
+    />
+
+  </div>
+</template>
+
+<script>
+import AddHolidays from './AddHolidays.vue';
+
+export default {
+  components: {
+    AddHolidays
+  },
+  name: 'HolidaysInfoLine',
+  props: [
+    'canton',
+    'holidays'
+  ]
+};
+
+</script>
+
+<style lang="scss">
+.comp-info-line {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+
+  .holidays-counter {
+    margin-top: 1rem;
+    margin-right: 1rem;
+    font-size: 1rem;
+  }
+
+  .add-all-button {
+    margin-top: 1rem;
+  }
+}
+</style>
