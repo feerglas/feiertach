3ecc5ecbf5e654b24403311aeb853fe2f6657869
feerglas <info@feerglas.com>
2020-10-19 17:57:02 +0200

    fix: remove scss scope, make class scope

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="layout-default">
 
     <Header />
 
@@ -10,19 +10,6 @@
 
         <transition name="fade" appear>
           <main>
-
-            <h1
-              v-if="title"
-              class="title is-1"
-            >
-              {{title}}
-              <Flag
-                :name="image"
-                v-if="image"
-                class="image"
-              />
-            </h1>
-
             <slot/>
 
           </main>
@@ -35,50 +22,42 @@
 </template>
 
 <script>
-import Flag from '../assets/flags/Index.vue';
 import Footer from '../components/Footer.vue';
 import Header from '../components/Header.vue';
 
 export default {
   components: {
-    Flag,
     Footer,
     Header
   },
-  props: [
-    'title',
-    'image',
-    'noPadding'
-  ]
+  props: ['noPadding']
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "../styles/bulma.scss";
 
-.title {
-  display: flex;
-  align-items: center;
-}
+.layout-default {
+  .title {
+    display: flex;
+    align-items: center;
+  }
 
-.image {
-  margin-left: 1rem;
-}
+  .fade-enter-active {
+    transition: opacity 300ms;
+  }
 
-.fade-enter-active {
-  transition: opacity 300ms;
-}
+  .fade-enter {
+    opacity: 0;
+  }
 
-.fade-enter {
-  opacity: 0;
-}
+  .container:not(.no-padding) {
+    @include defaultLayoutBleed();
+  }
 
-.container:not(.no-padding) {
-  @include defaultLayoutBleed();
-}
-
-.container.no-padding {
-  max-width: 100%;
+  .container.no-padding {
+    max-width: 100%;
+  }
 }
 
 </style>
