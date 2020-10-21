42ebde74285b4eeceb5acb93e54d22319d5651b4
feerglas <info@feerglas.com>
2020-10-21 12:27:03 +0200

    feat: add pwa config

--- gridsome.config.js
+++ gridsome.config.js
@@ -33,6 +33,47 @@ module.exports = {
         rewriteDefaultLanguage: true
       },
       use: 'gridsome-plugin-i18n'
+    },
+    {
+      options: {
+        appleMaskIconColor: '#fdfdfc',
+        backgroundColor: '#fdfdfc',
+        cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg,gif',
+        categories: [
+          'lifestyle',
+          'productivity',
+          'utilities'
+        ],
+        description: 'Schweizerische Feiertage',
+        dir: 'auto',
+        disableServiceWorker: false,
+        disableTemplatedUrls: false,
+        display: 'standalone',
+        icon: 'src/favicon.png',
+        lang: 'de-DE',
+        manifestPath: 'manifest.json',
+        maskableIcon: true,
+        msTileColor: '#fdfdfc',
+        screenshots: [
+          {
+            sizes: '1280x720',
+            src: 'screenshots/screenshot1.png',
+            type: 'image/png'
+          },
+          {
+            sizes: '1280x720',
+            src: 'screenshots/screenshot2.png',
+            type: 'image/png'
+          }
+        ],
+        serviceWorkerPath: 'service-worker.js',
+        shortName: 'feiertaCH',
+        startUrl: '/',
+        statusBarStyle: 'default',
+        themeColor: '#e85a4f',
+        title: 'feiertaCh'
+      },
+      use: 'gridsome-plugin-pwa'
     }
   ],
   siteDescription: 'List holidays from Switzerland',
