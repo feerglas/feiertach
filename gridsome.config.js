94c6959edacbc58fff6da4297b20912cb0e7464b
feerglas <info@feerglas.com>
2020-10-21 12:40:18 +0200

    fix: disable html caching

--- gridsome.config.js
+++ gridsome.config.js
@@ -38,7 +38,7 @@ module.exports = {
       options: {
         appleMaskIconColor: '#fdfdfc',
         backgroundColor: '#fdfdfc',
-        cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg,gif',
+        cachedFileTypes: 'js,json,css,png,jpg,jpeg,svg,gif',
         categories: [
           'lifestyle',
           'productivity',
