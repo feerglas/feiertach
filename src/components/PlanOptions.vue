07b5c0efab028627cd61f92ea5cdabeba960538c
feerglas <info@feerglas.com>
2020-10-28 17:34:26 +0100

    refactor: add component for plan options

---
+++ src/components/PlanOptions.vue
@@ -0,0 +1,65 @@
+<template>
+
+  <b-field
+    class="block plan-options"
+    :label="$t('plan.options')"
+  >
+    <b-checkbox
+      class="is-medium"
+      v-model="onlyOfficialSelection"
+      @change.native="optionsChange"
+      :disabled="disabled"
+    >
+      {{$t('plan.onlyOfficial')}}
+    </b-checkbox>
+
+    <b-checkbox
+      class="is-medium"
+      v-model="onlyAllCantonSelection"
+      @change.native="optionsChange"
+      :disabled="disabled"
+    >
+      {{$t('plan.onlyAllCanton')}}
+    </b-checkbox>
+  </b-field>
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      onlyAllCantonSelection: false,
+      onlyOfficialSelection: false
+    };
+  },
+  methods: {
+    optionsChange() {
+      this.$emit('changed', {
+        onlyAllCantonSelection: this.onlyAllCantonSelection,
+        onlyOfficialSelection: this.onlyOfficialSelection
+      });
+    }
+  },
+  name: 'PlanOptions',
+  props: [
+    'disabled',
+    'cantons',
+    'locale'
+  ]
+};
+
+</script>
+
+<style lang="scss">
+
+.block.plan-options .field.has-addons {
+  display: block;
+}
+
+.block.plan-options label.b-checkbox {
+  width: 100%;
+}
+
+</style>
