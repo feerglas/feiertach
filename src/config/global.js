8e9c147c4da4edb5a313eca79b62e23440297df5
Yves Tscherry <info@feerglas.com>
2021-11-06 15:29:18 +0100

    feat: make years array automatically starting from current year

--- src/config/global.js
+++ src/config/global.js
@@ -1,10 +1,13 @@
+const today = new Date();
+const year = today.getFullYear();
+
 const config = {
   planerDaySpanToIncludeForYearBeforeFirstAndAfterLast: 7,
   planerMaxFreeDays: 3,
   years: [
-    2020,
-    2021,
-    2022
+    year,
+    year + 1,
+    year + 2
   ]
 };
 
